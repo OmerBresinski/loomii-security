c8b530a40b2e39abce331bb5579f92151dcda33c
Omer Bresinski <omerbres@gmail.com>
2026-05-21 03:19:19 +0100

    reviews endpoint

--- apps/api/src/routes/v1/index.ts
+++ apps/api/src/routes/v1/index.ts
@@ -4,6 +4,7 @@ import { integrationRoutes } from "./integrations";
 import { searchRoutes } from "./search";
 import { threatModelRoutes } from "./threat-model";
 import { policyRoutes } from "./policies";
+import { reviewRoutes } from "./reviews";
 
 export const v1Routes = new Hono<AppEnv>();
 
@@ -12,6 +13,7 @@ v1Routes.route("/integrations", integrationRoutes);
 v1Routes.route("/search", searchRoutes);
 v1Routes.route("/threat-model", threatModelRoutes);
 v1Routes.route("/policies", policyRoutes);
+v1Routes.route("/reviews", reviewRoutes);
 
 /**
  * GET /api/v1/me - Return current authenticated user info.
